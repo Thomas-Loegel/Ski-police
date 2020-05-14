61626f4bb21edba6c372cae773f886717a0d4397
Thomas Loegel <t.loegel@gmail.com>
2020-05-14 10:48:16 +0200

    create template

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -13,13 +13,6 @@ class TestController extends AbstractController
      */
     public function index()
     {
-        // $repo = $this->getDoctrine()->getRepository(Competitor::class);
-        // $competitor = $repo->find(9);
-
-        // $em = $this->getDoctrine()->getManager();
-        // $em->remove($competitor);
-        // $em->flush();
-
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
         ]);
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,48 +7,13 @@ use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Team;
 use App\Entity\Competitor;
 use App\Entity\Time;
+use App\Entity\Edition;
 
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-
-        for ($v=0; $v < 3; $v++) { 
-            $team = new Team();
-            $team->setTeamName("Team ".$v)
-            ->setCity("city ".$v);
-            // ->addCompetitor($competitor);
-
-            $manager->persist($team);
-
-            for ($i=0; $i < 4; $i++) { 
-                $number = $i*($v+1);
-                $competitor = new Competitor();
-                $competitor->setFirstName("Prénom ".$number)
-                ->setLastName("Nom ".$number)
-                ->setBirthYear(1986)
-                ->setSexe("F")
-                ->setStatus("Police Municipale")
-                ->AddTeam($team);
-    
-                $manager->persist($competitor);
-
-                for ($f=0; $f < 3; $f++) { 
-                    $number = $f*($i+1);
-                    $time = new Time();
-                    $time->setAttemp1(15500)
-                    ->setAttemp2(12500)
-                    ->setJerseyNumber($number);
-
-                    $manager->persist($time);
-                }
-            }
-        }
-
-        $manager->flush();
+     
     }
 }
